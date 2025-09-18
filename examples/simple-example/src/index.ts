ebcbe50ba3b591848346a0f9aed088a25f3cb8d0
devzolo <zolotorevsky@gmail.com>
2025-09-18 10:31:52 -0300

    chore: Remove openapi.json file and update OpenAPI generation path in example project
    - Deleted the openapi.json file from the examples directory.
    - Updated the path for OpenAPI generation in the index.ts file to reflect the new structure.

--- examples/simple-example/src/index.ts
+++ examples/simple-example/src/index.ts
@@ -1,7 +1,7 @@
-import ApiServer from "@ts-api-kit/core/server";
 import { generateOpenAPI } from "@ts-api-kit/compiler";
+import ApiServer from "@ts-api-kit/core/server";
 
 const server = new ApiServer(3000);
 await server.configureRoutes(`${import.meta.dirname}/routes`);
-generateOpenAPI(`${import.meta.dirname}/routes`, "../openapi.json");
+generateOpenAPI(`${import.meta.dirname}/routes`, "./openapi.json");
 server.start();
