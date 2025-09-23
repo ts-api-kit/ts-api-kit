01f455dd3a326abe909174c61d53f049b7163f0f
devzolo <zolotorevsky@gmail.com>
2025-09-23 10:25:22 -0300

    chore(dependencies): update package and restructure peer dependencies

--- examples/simple-example/src/routes/+route.ts
+++ examples/simple-example/src/routes/+route.ts
@@ -2,31 +2,32 @@ import { handle } from "@ts-api-kit/core";
 import { response } from "@ts-api-kit/core/openapi";
 
 interface User {
-	id: number;
-	name: string;
-	email: string;
-	parent?: User;
+  id: number;
+  name: string;
+  email: string;
+  parent?: User;
 }
 
 interface HelloWorldResponse {
-	user: User;
+  user: User;
 }
 
 export const GET = handle(
-	{
-		openapi: {
-			responses: {
-				200: response.of<HelloWorldResponse>(),
-			},
-		},
-	},
-	({ response }) =>
-		response.ok({
-			user: {
-				id: 1,
-				name: "John Doe",
-				email: "john.doe@example.com",
-				parent: { id: 2, name: "Jane Doe", email: "jane.doe@example.com" },
-			},
-		}),
+  {
+    openapi: {
+      responses: {
+        200: response.of<HelloWorldResponse>(),
+      },
+    },
+  },
+  ({ response }) => {
+    return response.ok({
+      user: {
+        id: 1,
+        name: "John Doe",
+        email: "john.doe@example.com",
+        parent: { id: 2, name: "Jane Doe", email: "jane.doe@example.com" },
+      },
+    });
+  }
 );
